828df874e9fdd15dc45000a093163793e19685c6
jonatas <jonatas_dev@hotmail.com>
2025-06-05 19:08:57 -0300

    Criação de cliente entity.

--- src/main/java/br/com/api/CadastroDeClientes/controller/ClienteController.java
+++ src/main/java/br/com/api/CadastroDeClientes/controller/ClienteController.java
@@ -1,4 +1,4 @@
-package br.com.api.CadastroDeClientes;
+package br.com.api.CadastroDeClientes.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class ClienteController {
 
     //Método para boas vindas.
     @GetMapping("/boasvindas")
---
+++ src/main/java/br/com/api/CadastroDeClientes/model/ClienteModel.java
@@ -0,0 +1,49 @@
+package br.com.api.CadastroDeClientes.model;
+
+import jakarta.persistence.*;
+
+//Entity transforma a classe em uma entidade do BD.
+@Entity
+@Table(name = "tb_cadastro")
+public class ClienteModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public ClienteModel() {
+    }
+
+    public ClienteModel(String nome, String email, int idade) {
+        this.nome = nome;
+        this.email = email;
+        this.idade = idade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
